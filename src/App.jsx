ee70ff47454e9e677f65c8c3dac747e4b943c3bc
VansRouges <evansagina57@gmail.com>
2022-12-03 13:25:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { CssBaseline, Grid } from '@material-ui/core'
+import './App.css'
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
+function App() {
+
+  return (
+    <main>
+      <CssBaseline />
+      <Header />
+      <Grid>
+        
+      </Grid>
+    </main>
+  )
+}
+
+export default App
